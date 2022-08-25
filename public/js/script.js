c8217a173189d9a8d4fc42d88163bb2728382de5
krisassaurus <orelk@sheridancollege.ca>
2022-08-25 18:47:48 -0400

    Add precipitation percentage information

--- public/js/script.js
+++ public/js/script.js
@@ -3,6 +3,7 @@ const submit = document.getElementById('submit');
 const outputPar1 = document.querySelector('#weather-output p:first-child');
 const outputPar2 = document.querySelector('#weather-output p:nth-child(2)');
 const outputPar3 = document.querySelector('#weather-output p:nth-child(3)');
+const outputPar4 = document.querySelector('#weather-output p:nth-child(4)');
 
 submit.addEventListener('click', (event) => {
   event.preventDefault();
@@ -22,6 +23,7 @@ function getWeatherData(address) {
       outputPar1.textContent = `${data.location}`;
       outputPar2.textContent = `Temperature: ${data.temperature}℃`;
       outputPar3.textContent = `Feels Like: ${data.feelslike}℃`;
+      outputPar4.textContent = `Precipitation chance is: ${data.precip}%`;
     });
 }
 
